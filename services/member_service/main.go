df16f796ba1375780a15f5b8fce0836cbd06d76c
Alvaro <3046971840116@ingenieria.usac.edu.gt>
2024-04-05 14:11:38 -0600

    f commit

---
+++ services/member_service/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"gomonorepo/shared/models"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		// create a doctor from the models package from shared
+		doctor := models.Doctor{
+			Uid: "123",
+			Credentials: models.Credentials{
+				Email:    "ga1318garcia@gmail.com",
+				Password: "password",
+			},
+			Profile: models.Profile{
+				Uid:        "123",
+				Name:       "Gabe",
+				Email:      "ga1318garcia@gmail.com",
+				Phone:      "123-456-7890",
+				Speciality: []string{"General"},
+				DoctorID:   "1",
+				Role:       "Doctor",
+			},
+			Status: "Active",
+			Clinics: []models.Clinics{
+				{
+					Id:     "1",
+					Status: "Active",
+				},
+			},
+		}
+
+		return c.JSON(doctor)
+	})
+
+	app.Listen(":8080")
+}
